60f7c3415f876e97d89552aa1a399e7ce9a6942e
aidatorajiro <kawarusosu@zoho.com>
2017-11-12 15:27:27 +0900

    use OrthographicCamera

--- src/Game.js
+++ src/Game.js
@@ -6,37 +6,46 @@ class Game {
   constructor () {
     this.pids = Pid.get_pids()
 
-    this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 )
-    this.camera.position.z = 1
-  
+    this.camera = new THREE.OrthographicCamera( this.width/-2, this.width/2, this.height/2, this.height/-2, 1, 2000 )
+    this.camera.position.z = 500;
+
     this.scene = new THREE.Scene()
   
-    this.geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 )
+    this.geometry = new THREE.PlaneGeometry( 100, 100, 1, 1 )
     this.material = new THREE.MeshNormalMaterial()
   
     this.mesh = new THREE.Mesh( this.geometry, this.material )
+    this.mesh.position.z = 0
     this.scene.add( this.mesh )
   
     this.renderer = new THREE.WebGLRenderer( { antialias: true } )
-    this.renderer.setSize( window.innerWidth, window.innerHeight )
+    this.renderer.setSize( this.width, this.height )
     document.body.appendChild( this.renderer.domElement )
 
-    window.addEventListener('resize', () => { this.resize() }, false );
+    window.addEventListener('resize', () => { this.resize() }, false )
 
     this.animate()
   }
 
+  get width () {
+    return window.innerWidth
+  }
+
+  get height () {
+    return window.innerHeight
+  }
+
   resize () {
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    this.camera.aspect = window.innerWidth / window.innerHeight;
-    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(this.width, this.height)
+    this.camera.left   = this.width/-2
+    this.camera.right  = this.width/2
+    this.camera.top    = this.height/2
+    this.camera.bottom = this.height/-2
+    this.camera.updateProjectionMatrix()
   }
 
   animate () {
     requestAnimationFrame( () => { this.animate() } );
-    
-    this.mesh.rotation.x += 0.01;
-    this.mesh.rotation.y += 0.02;
     
     this.renderer.render( this.scene, this.camera );
   }
