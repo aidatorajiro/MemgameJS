c0053dc5acfde5ed1c70ce1fb28b13eb4e3bc727
aidatorajiro <kawarusosu@zoho.com>
2018-05-13 22:18:36 +0900

    set fullscreen on start

--- main.js
+++ main.js
@@ -15,6 +15,8 @@ function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600, backgroundColor: '#000000'})
 
+  mainWindow.setFullScreen(true)
+
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
